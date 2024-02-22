f194c127a5c581a8e035b3e24880c1699432d988
yingwang.long <2570715747@qq.com>
2024-02-22 17:11:10 +0800

    将整数换成小数，使运算符左右两边类型相同

--- R.frag.glsl
+++ R.frag.glsl
@@ -1,19 +1,18 @@
-precision lowp float;
+precision mediump float;
 
-uniform lowp float BlendIntensity;
+uniform mediump float BlendIntensity;
 varying mediump vec2 vTexCoord;
-uniform mediump float kzTime;
 uniform mediump vec2 kzWindowSize;
 
 //  A-------B
 //  |       |
 //  D-------C
 //  上边缘LineAB    下边缘LineDC    左边缘LineAD    右边缘LineBC
-bool isPinterCircle(vec2 p, float r, vec2 st) {
-    return pow((st.x - p.x), 2) + pow((st.y - p.y), 2) <= pow(r, 2);
+bool pinterCircle(vec2 p, float r, vec2 st) {
+    return pow((st.x - p.x), 2.0) + pow((st.y - p.y), 2.0) <= pow(r, 2.0);
 }
 
-bool isPinterLine(vec2 p1, vec2 p2, float lineWidth, vec2 st){
+bool pinterLine(vec2 p1, vec2 p2, float lineWidth, vec2 st){
 
     if (p1.x > p2.x){
         vec2 temp = p1;
@@ -21,32 +20,33 @@ bool isPinterLine(vec2 p1, vec2 p2, float lineWidth, vec2 st){
         p2 = temp;   
     }
     
+    
     if (p1.x <= p2.x && p1.y <= p2.y){
-        float d_chuyi_2sita = lineWidth / (2 * cos(atan((p2.y - p1.y)/(p2.x - p1.x))));
+        float d_chuyi_2sita = lineWidth / (2.0 * cos(atan((p2.y - p1.y)/(p2.x - p1.x))));
         float k = (p2.y - p1.y)/(p2.x - p1.x) * (st.x - p1.x);
 
-        return st.y <= k + p1.y + d_chuyi_2sita &&//上边缘LineAB
+        return st.y <= k + p1.y + d_chuyi_2sita &&//上边缘LineAB    d是线条宽度
                st.y >= k + p1.y - d_chuyi_2sita &&//下边缘LineDC
-               st.y >= -1/k * (st.x - p1.x) + p1.y &&//左边缘LineAD
-               st.y <= -1/k * (st.x - p2.x) + p2.y;  //右边缘LineBC
+               st.y >= -1.0/k * (st.x - p1.x) + p1.y &&//左边缘LineAD
+               st.y <= -1.0/k * (st.x - p2.x) + p2.y;  //右边缘LineBC
 
     }else if (p1.x <= p2.x && p1.y >= p2.y){
-        float d_chuyi_2sita = lineWidth / (2 * cos(atan((p2.y - p1.y)/(p2.x - p1.x))));
+        float d_chuyi_2sita = lineWidth / (2.0 * cos(atan((p2.y - p1.y)/(p2.x - p1.x))));
         float k = (p2.y - p1.y)/(p2.x - p1.x) * (st.x - p1.x);
 
         return st.y <= k + p1.y + d_chuyi_2sita &&//上边缘LineAB
                st.y >= k + p1.y - d_chuyi_2sita &&//下边缘LineDC
-               st.y <= -1/k * (st.x - p1.x) + p1.y &&//左边缘LineAD
-               st.y >= -1/k * (st.x - p2.x) + p2.y;  //右边缘LineBC
+               st.y <= -1.0/k * (st.x - p1.x) + p1.y &&//左边缘LineAD
+               st.y >= -1.0/k * (st.x - p2.x) + p2.y;  //右边缘LineBC
                
     }else{
-        return 0;
+        return false;
     }
 }
 
-bool isPinterPL(vec2 p1, vec2 p2, float lineWidth, vec2 st)
+bool pinterPL(vec2 p1, vec2 p2, float lineWidth, vec2 st)
 {
-    return isPinterLine(p1, p2, lineWidth, st) || isPinterCircle(p1, lineWidth / 2, st) || isPinterCircle(p2, lineWidth / 2, st);
+    return pinterLine(p1, p2, lineWidth, st) || pinterCircle(p1, lineWidth / 2.0, st) || pinterCircle(p2, lineWidth / 2.0, st);
 }
 
 void main() {
@@ -54,10 +54,10 @@ void main() {
     float px = 1.0 / kzWindowSize.y;
     
     vec3 color = vec3(1.0);
-    if (isPinterPL(vec2(0.05,0.05), vec2(0.509,0.509), 0.01, st)){
-        color = vec3(0.0,1.0,0.0);
-    }else if(isPinterPL(vec2(0.509,0.509), vec2(0.8,0.05), 0.01, st)){
-        color = vec3(0.0,1.0,0.0);
+    if (pinterPL(vec2(0.05,0.05), vec2(0.509,0.509), 0.01, st)){
+        color = vec3(0.17, 0.86, 0.48);
+    }else if(pinterPL(vec2(0.509,0.509), vec2(0.8,0.05), 0.01, st)){
+        color = vec3(0.17, 0.86, 0.48);
     }
 
     gl_FragColor = vec4(color, 1.0);
--- R.vert.glsl
+++ R.vert.glsl
@@ -1,5 +1,5 @@
 attribute vec3 kzPosition;
-uniform highp mat4 kzProjectionCameraWorldMatrix;
+uniform mediump mat4 kzProjectionCameraWorldMatrix;
 attribute vec2 kzTextureCoordinate0;
 
 varying vec2 vTexCoord;
@@ -9,6 +9,7 @@ void main()
     precision mediump float;
     vTexCoord = kzTextureCoordinate0;
 
-    // kzPosition等是kanzi自动映射好的。
     gl_Position = kzProjectionCameraWorldMatrix * vec4(kzPosition.xyz, 1.0);
 }
+
+
